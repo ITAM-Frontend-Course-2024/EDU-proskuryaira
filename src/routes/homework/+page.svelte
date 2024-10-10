f46d95262a58915bda88897c7792bec82b1cfd36
Irinaaaaaaaaaaa <proskuryakova0404@mail.ru>
2024-10-10 14:09:45 +0300

    update message

---
+++ src/routes/homework/+page.svelte
@@ -0,0 +1,253 @@
+<header>
+     <h1><span class="book_name">The Secret History</span> Donna Tartt</h1>
+    <nav class="menu">
+        <ul>
+          <li><a href="#section1">О чем сайт</a></li>
+          <li><a href="#section2">Сюжет</a></li>
+          <li><a href="#section3">Библиография Донны Тартт</a></li>
+          <li><a href="#section4">Форма обратной связи</a></li>
+        </ul>
+      </nav>
+</header>
+<main>
+    <article>
+        <section id="section1">
+            <h2>О чем сайт</h2>
+            <div> <strong><q>Тайная история</q></strong> - это одна из моих любимых книг, а также первый роман потрясающей современной писательницы Донны Тартт. Книга переведена на двадцать четыре языка и продолжает расходиться многомиллионными тиражами во всем мире.</div>
+        </section>
+        <section id="section2">
+            <h2>Сюжет</h2>
+            <div class="main_part">
+                <figure>
+                    <img class="main_img" src="https://harpersbazaaruk.cdnds.net/15/37/2048x2730/2048x2730-donna-tartt-square-jpg-f5e2fd93.jpg" alt="a">
+                   </figure>
+                <div class="plot">
+                    <p>Действие романа разворачивается не на родном американском Юге, который станет местом действия второго романа Тартт «Маленький друг», а в Новой Англии, в колледже Хэмпден, во многих отношениях напоминающем Беннингтон. Основанный на матрице университетского романа, он повествует о тесно сплоченной группе из шести студентов, изучающих древнегреческий язык.</p>
+                    <p>Первоначально роман назывался «Бог иллюзий» (на некоторых языках до сих пор так), о чём сама писательница говорит в интервью. Такое название имело основание, поскольку роман исследует, как именно влияют университетское образование и художественные тексты на формирование личности. Название «Тайная история» отсылает к произведению византийского историка Прокопия Кесарийского. Исполняя обязанности военного летописца при дворе императора Юстиниана, он написал восьмитомную «Историю войн», которую снабдил «неофициальным» дополнением — памфлетом под заглавием «Anecdota». В нем он излагает закулисные причины описанных в «Истории» политических событий и весьма критически оценивает действия императора. Греческое название памфлета переводилось на английский как «The Secret History», а на русский — как «Тайная история».</p>
+                </div>                  
+            </div>
+        </section>
+        <section id="section3">
+            <h2 class="h_years">Библиография Донны Тартт</h2>
+            <ul class="years">
+                <li><span class="year">1992</span> <span class="book_name" id="book">Тайная история </span></li>
+                <li> <span class="year">2002</span> <span class="book_name" id="book">Маленький друг</span></li>
+                <li> <span class="year">2013</span> <span class="book_name" id="book">Щегол</span></li>
+            </ul>
+        </section>
+        <section id="section4">
+            <h2>Форма обратной связи</h2>
+            <form action="">
+                <label for="name">ФИО</label>
+                <input placeholder = "Введите ваше ФИО" id ="name">
+                <label for="phone">Номер телефона</label>
+                <input placeholder = "Номер телефона" id ="phone" type="tel">
+                <label for="email">Почта</label>
+                <input placeholder = "1234@mail.ru" id ="email" type="email">
+                <label for="city">Город</label>
+                <select name="" id="city">
+                    <option value="">Москва</option>
+                    <option value="">Казань</option>
+                    <option value="">Лондон</option>
+                </select>
+                <span>
+                    <label for="sex1">мужчина</label>
+                    <input id = "sex1" type="radio" name = "val">
+                    <label for="sex2">женщина</label>
+                    <input id = "sex2" type="radio" name = "val">
+                    <label for="sex3">другое</label>
+                    <input id = "sex3" type="radio" name = "val">
+                </span>
+                <div><button type="button">Отправить</button></div>
+            </form>
+        </section>
+    </article>
+</main>
+<footer>
+    <span class="f_item">
+        <span>Телеграм: <a class="link"  href="https://t.me/proskuryaira">tg:proskuryaira</a></span>
+        <img class = "icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/1200px-Telegram_2019_Logo.svg.png" alt="">
+    </span>
+    <span class="f_item">
+        <span>Почта: <a class="link"  href="https://t.me/proskuryaira">proskuryakova0404@mail.ru</a></span>
+        <img class = "icon" src="https://play-lh.googleusercontent.com/IZ1LeYReXw0XIMBrb5_XCHIuH6qt1gNXT1QqdtY4MFB_QjF2DlKyv42douMSYenZnQ" alt="">
+    </span>
+    <span class="f_item">
+        <a class="link"  href="https://t.me/proskuryaira">Проскурякова Ирина 2024 </a>
+        <img class="icon" src="https://i.pinimg.com/564x/6f/f0/3d/6ff03d9b05ef19a761a821983a8ce68f.jpg" alt="">
+    </span>
+</footer>
+
+
+
+<style>
+    html, header, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, img, ins, kbd, s, samp,
+    small, strike, sub, sup, tt, var,
+    b, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+    }
+     article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+    body {
+        line-height: 1;
+    }
+    ol, ul {
+        list-style: none;
+    }
+    blockquote, q {
+        quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    } 
+/* конец сброса настроек */
+
+    header{
+        background-image: url("https://i.pinimg.com/564x/02/5a/d0/025ad03d613d84c2597cbbb849df2b7a.jpg");
+        height: 174px;
+    }
+    h1{
+        font-family: "Updock", cursive;
+        font-size: 45px;
+        color: rgb(245, 239, 219);
+        text-align: center;
+        text-transform:capitalize;
+        padding: 17px;
+        font-weight: 500;
+        /* text-shadow: rgb(184, 19, 19) 0 -2px; */
+    }  
+    .book_name::before {
+        content: open-quote;
+    }
+    .book_name::after {
+        content: close-quote;
+    } 
+
+    ul {
+        display: flex;
+        margin: 15px; 
+        padding: 4px; 
+        text-align: center;
+        flex-direction: row;
+        justify-content: space-around;      
+   }
+   ul li {
+    display: flex; 
+    margin-right: 5px; 
+    padding: 3px; 
+    flex-direction: column;
+    gap: 10px ;
+    font-size: 18px;
+    
+   }
+   #section1{
+    padding: 15px;
+   }
+    a {
+        color:  rgb(245, 239, 219);
+    }
+
+
+    main{
+        background-color: rgb(0, 0, 0);
+        color: #ddb091;
+    } 
+    h2{
+
+        font-size: 33px;
+        font-weight: bold;
+        text-align: center;
+        font-family: "Great Vibes", cursive;
+        /* text-shadow: rgb(69, 0, 0) 0 -2px; */
+        /* text-shadow: -1px -3px #ddb091; */
+    }
+    section{
+        padding: 10px;
+    }
+    .main_part{
+        display: grid;
+        align-items: center; 
+        grid-template-columns: 252px auto;
+        column-gap: 5px;
+        align-items: start;    
+    }
+    .plot{
+        padding: 16px;
+        font-size: 15px;
+    }
+    .main_img{
+        max-width: 100%;
+        max-height:100%;
+    }
+    p{
+        margin: 10px;
+    }
+
+    #section3{
+        background-image: url("https://i.pinimg.com/564x/bc/fc/0b/bcfc0bb3fb4a634b57a2e1e9ae0b6850.jpg");
+        color: rgb(245, 239, 219);
+    }
+    .h_years{
+        font-size: 37px;
+    }
+    .year{
+        font-size: 49px;
+        font-family: "Great Vibes", cursive;
+    }
+    #book{
+        font-size: 22px;
+    }
+    
+
+    form{
+        padding: 20px;
+        text-align: center;
+    }
+    button{
+        background-color: rgb(245, 239, 219);
+        padding: 8px;
+        margin: 10px;
+    }
+
+    footer{
+        background-color: rgb(0, 0, 0);
+        color: #ddb091;
+        display: flex;
+        text-align: center;
+        justify-content: center;
+    }
+    .icon{
+        width: 17px;
+        position: relative;
+        bottom: -4px;
+    }
+    .f_item{
+        padding: 15px;
+    }
+    .link{
+        color: #ddb091;    
+    }
+</style>
