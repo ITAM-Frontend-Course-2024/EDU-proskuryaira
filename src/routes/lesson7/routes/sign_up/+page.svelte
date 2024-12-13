54c3dd45ef9890ce843dd0eec388f05cda7a4f54
Irinaaaaaaaaaaa <proskuryakova0404@mail.ru>
2024-12-13 19:04:21 +0300

    change

---
+++ src/routes/lesson7/routes/sign_up/+page.svelte
@@ -0,0 +1,157 @@
+<script lang="ts">
+	import "../../app.css";
+	// import { goto } from '@sveltejs/kit/navigation';
+	// import { goto } from '$app/navigation';
+	import { api } from "../api.js";
+
+	let password: string = "";
+	let email: string = "";
+	let name: string = "";
+	let surname: string = "";
+	let tg: string = "";
+
+	let selectedValue = "yes"; // Значение по умолчанию
+	function handleSubmit() {
+		if (selectedValue === "yes") {
+			// Переход на страницу заполнения формы
+			window.location.href = "/form";
+		} else {
+			// Переход на главную страницу
+			window.location.href = "/";
+		}
+	}
+	// import { onMount } from "svelte";
+	// let api: string = "http://193.227.241.239:8080/user/create";
+
+	let id: number;
+	async function create_user() {
+		try {
+			let response = await fetch(api + "/user/create", {
+				method: "POST",
+				body: JSON.stringify({ PWD: password, email: email, name: name, surname: surname, tg: tg }),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			let obj = await response.json();
+			console.log(obj);
+			id = obj.id;
+			// window.location.href = "/project";
+			// он как-то очень долго грузит следующую страницу. я не уверена, что сделала правильно функцию handleSubmit()
+			handleSubmit();
+		} catch (error) {
+			console.error("Ошибка при запросе:", error);
+		}
+	}
+</script>
+
+<header>
+	<img height="24px" src="/cover.png" alt="" style="margin-left:15px" />
+</header>
+<h1>Регистрация</h1>
+<main>
+	<form action="">
+		<div class="question">
+			<div><label for="email">Почта</label></div>
+			<div><input bind:value={email} placeholder="1234@mail.ru" id="email" type="email" /></div>
+		</div>
+		<div class="question">
+			<div><label for="password">Пароль</label></div>
+			<div>
+				<input bind:value={password} placeholder="Введите пароль" id="password" type="password" />
+			</div>
+		</div>
+		<div class="question">
+			<div><label for="name">Имя</label></div>
+			<div><input bind:value={name} placeholder="Иван" id="name" type="text" /></div>
+		</div>
+		<div class="question">
+			<div><label for="surname">Фамилия</label></div>
+			<div><input bind:value={surname} placeholder="Иванов" id="surname" type="text" /></div>
+		</div>
+		<div class="question">
+			<div><label for="tg">Телеграмм</label></div>
+			<div><input bind:value={tg} placeholder="@meow" id="tg" type="text" /></div>
+		</div>
+		<fieldset>
+			<legend>Создать анкету:</legend>
+			<label for="yes">да</label>
+			<input
+				class="choice"
+				id="yes"
+				type="radio"
+				name="val"
+				value="yes"
+				bind:group={selectedValue}
+			/>
+			<label for="no">нет</label>
+			<input class="choice" id="no" type="radio" name="val" value="no" bind:group={selectedValue} />
+		</fieldset>
+
+		<div class="submit">
+			<input type="submit" on:click={create_user} value="Зарегистрироваться" id="submit" />
+		</div>
+	</form>
+</main>
+
+<style lang="scss">
+	h1 {
+		display: flex;
+		justify-content: center;
+		margin: 12px;
+		font-size: 70px;
+	}
+
+	main {
+		display: flex;
+		justify-content: center;
+
+		form {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			background-color: #1e1e1e;
+			width: 578px;
+			height: 572px;
+			border-radius: 8px;
+			margin: 15px;
+
+			input {
+				background-color: #1e1e1e;
+				width: 530px;
+				border-radius: 8px;
+				color: aliceblue;
+				height: 40px;
+			}
+			fieldset {
+				display: flex;
+				justify-content: center;
+				margin: 10px;
+				width: 300px;
+				height: 60px;
+				.choice {
+					margin: 10px;
+					height: 10px;
+					width: auto;
+				}
+			}
+
+			.question {
+				margin: 10px;
+			}
+
+			#submit {
+				background-color: rgba(245, 245, 245, 1);
+				height: 190%;
+				width: 310px;
+				border-radius: 8px;
+				color: black;
+			}
+		}
+	}
+</style>
---
+++ src/routes/project1/data.js
@@ -0,0 +1,33 @@
+export const posts = [
+	{
+		id: 1234,
+		name: 'Nastya',
+        // image: 'https://thumbs.dfs.ivi.ru/storage38/contents/7/1/6c980cc5613cbbdbb31b15c9afb7a5.jpg',
+        image: '/cilian.jpg',
+		description:
+			'Дизайнер. Ищу команду блабла бла. '
+	},
+
+	{
+		id: 5678,
+		name: 'Anna',
+        image: '/cilian.jpg',
+		description:
+			' align-content Свойство распределяет свободное пространство по поперечной оси между рядами флекс-элементов. Предположим, у вас 11 элементов в 3 рядах. Если размер родителя по поперечной оси позволяет, то при помощи align-content можно распределять строчки элементов: по верхнему краю, по нижнему, по центру или равномерно.'
+	},
+
+	{
+		id: 8901,
+		name: 'Victoria',
+        // image: 'https://uznayvse.ru/person/cillian-murphy/cillian_murphy03.jpg',
+        image: '/cilian.jpg',
+		description: "blah blah bla While safety is one of many Enrichment Center Goals,"
+	},
+    {
+		id: 2231,
+        // image: 'https://uznayvse.ru/person/cillian-murphy/cillian_murphy03.jpg',
+        image: '/cilian.jpg',
+		name: 'Игорь',
+		description: "We hope your brief detention in the relaxation vault has been a p blah blah bla While safety is one of many Enrichment Center Goals,"
+	}
+];
